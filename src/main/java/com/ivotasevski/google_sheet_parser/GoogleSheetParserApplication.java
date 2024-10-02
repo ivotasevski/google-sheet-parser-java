5772f92cc1662a974bb19fc51eb74acc81a2a274
Ivo Tasevski <ivo.tasevski@qinshift.com>
2024-10-02 11:23:54 +0200

    Initial project setup

---
+++ src/main/java/com/ivotasevski/google_sheet_parser/GoogleSheetParserApplication.java
@@ -0,0 +1,13 @@
+package com.ivotasevski.google_sheet_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSheetParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSheetParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivotasevski/google_sheet_parser/GoogleSheetParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivotasevski.google_sheet_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSheetParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
